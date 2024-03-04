98b652d34d0d0b2b17f958c9e3dd88cfc58331c8
mythlesh <mythlesh114@gmail.com>
2024-03-04 14:36:44 +0545

    Updates

---
+++ precache-manifest.f91b549ccd03dfa8007afda96191d3ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21ad24c3cedeedbd28b712c3397a5ffc",
+    "url": "/Neomorphism/index.html"
+  },
+  {
+    "revision": "b0e7de28c3aa47539ff9",
+    "url": "/Neomorphism/static/css/main.252e538b.chunk.css"
+  },
+  {
+    "revision": "8d777b2bc553b51df34d",
+    "url": "/Neomorphism/static/js/2.d92ec8a3.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/Neomorphism/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "b0e7de28c3aa47539ff9",
+    "url": "/Neomorphism/static/js/main.8b113eb7.chunk.js"
+  },
+  {
+    "revision": "c180365ac6e6bde98a1c",
+    "url": "/Neomorphism/static/js/runtime~main.d8a00d39.js"
+  }
+]);
